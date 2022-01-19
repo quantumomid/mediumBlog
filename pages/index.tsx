cca0f4912868af43c4d9fb20eaef505fe5923ed6
quantumomid <omid.wakili@outlook.com>
2022-01-19 19:43:02 +0000

    add heading section in home page and style with tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,25 @@ import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="">
-      <Header />
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium 2.0</title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
+      <Header />
 
-        <h1 className="">Medium 2.0</h1>
-
+      <main>
+        <section className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+          <div className="px-10 space-y-5">
+            <h1 className="text-6xl max-w-xl font-serif">
+              <span className="underline decoration-black decoration-4">Medium</span> is a place to write, read and connect
+            </h1>
+            <h2>It's easy and free to post your thinking on any topic and connect with millions of readers.</h2>
+          </div>
+          <img className="hidden md:inline-flex h-32 lg:h-full" src="/images/mediumLetterLogo.png" alt="Medium logo with a capital bold letter M" />
+        </section>
+        {/* Posts */}
+      </main>
     </div>
   )
 }
