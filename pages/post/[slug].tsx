567625257789e474d85199ee4752627ec454c048
quantumomid <omid.wakili@outlook.com>
2022-01-21 16:20:55 +0000

    connect form inputs with react hook form

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -78,6 +78,9 @@ interface IFormInput {
 const Post: React.FC<PostProps> = ({ post }) => {
     console.log({post});
     const { register, handleSubmit, formState: { errors } } = useForm<IFormInput>();
+    const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+        console.log({data});
+    };
 
     return (
         <main>
@@ -127,23 +130,69 @@ const Post: React.FC<PostProps> = ({ post }) => {
                 </div>
             </article>
             <hr className="max-w-lg my-5 mx-auto border-yellow-500" />
-            <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
                 <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
                 <h4 className="text-3xl font-bold">Leave a comment below!</h4>
                 <hr className="py-3 mt-2" />
 
+                {/* Hidden dummy input to register/connect to react hooks form */}
+                <input 
+                    {...register("_id")}
+                    type="hidden"
+                    name="_id"
+                    value={post._id}
+                />
+
                 <label className="block mb-5" htmlFor="name">
                     <span className="text-gray-700">Name</span>
-                    <input id="name" className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" placeholder="Omid Wakili" type="text" />
+                    <input 
+                        id="name" 
+                        className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"                         
+                        placeholder="Omid Wakili" 
+                        type="text" 
+                        {...register("name", { required: true })}
+                    />
                 </label>
                 <label className="block mb-5" htmlFor="email">
                     <span className="text-gray-700">Email</span>
-                    <input id="email" className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" placeholder="omid@hotmail.com" type="text" />
+                    <input 
+                        id="email" 
+                        className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+                        placeholder="omid@hotmail.com" 
+                        type="text" 
+                        {...register("email", { required: true })}
+                    />
                 </label>
                 <label className="block mb-5" htmlFor="comment">
                     <span className="text-gray-700">Comment</span>
-                    <textarea id="comment" className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring" placeholder="Great points!" rows={8} />
+                    <textarea 
+                        id="comment" 
+                        className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+                        placeholder="Great points!" 
+                        rows={8} 
+                        {...register("comment", { required: true })}
+                    />
                 </label>
+
+                {/* Displaying Errors */}
+                <div className="flex flex-col p-5">
+                    { errors.name && (
+                            <span className="text-red-500">- The Name field is required</span>
+                        ) 
+                    }
+                    { errors.email && (
+                            <span className="text-red-500">- The Email field is required</span>
+                        ) 
+                    }
+                    { errors.comment && (
+                            <span className="text-red-500">- The Comment field is required</span>
+                        ) 
+                    }
+                </div>
+                <input 
+                    type="submit" 
+                    className="shadow bg-yellow-500 hover:bg-yellow-400 cursor-pointer focus:shadow-outline focus-outline-none text-white font-bold py-2 px-4 rounded" 
+                />
             </form>
         </main>
     );
